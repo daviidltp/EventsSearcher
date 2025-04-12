ed3f92ec3641de99bc35e0fa8d5d95c00b138b4f
Angel <angelgallego999@gmail.com>
2025-04-12 14:41:39 +0200

    Seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import compressor from "astro-compressor";
+
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+      minify: true,
+    }
   },
-
+  build: {
+    inlineStylesheets: 'auto', // Mejor rendimiento
+  },
+  compressHTML: true,
   base: '',
-  integrations: [react()],
+  integrations: [
+    react(),
+    compressor(),
+	sitemap()
+  ],
 });
