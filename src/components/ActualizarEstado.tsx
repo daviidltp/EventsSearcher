9e612be5c356bcfee1fe8f006eb0cce6913ab0f4
daviidltp <davidllopezca@gmail.com>
2025-04-12 19:53:22 +0200

    Mapa

--- src/components/ActualizarEstado.tsx
+++ src/components/ActualizarEstado.tsx
@@ -141,7 +141,7 @@ export default function ActualizarEstado() {
               onChange={(e) => setEstado(e.target.value)}
               className="w-full border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-black dark:text-white p-2 rounded"
             >
-              <option value="OK">En marcha</option>
+              <option value="OK">Correcto</option>
               <option value="Directo">En directo</option>
               <option value="Cancelada por lluvia">Cancelada por lluvia</option>
               <option value="Retrasada">Retrasada</option>
